66e07d8eea7859d2d6858d45a76821f810a4b206
Alex <al222111377@gmail.com>
2022-10-28 23:49:42 -0500

    creacion de seerder y factory se modifico la base de datos por las llaves foraneas

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,14 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::resource('productos', ProductoController::class)->middleware('auth');
 Route::resource('marcas', MarcaController::class)->middleware('auth');
